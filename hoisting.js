e90f0234e3f4c91f0f1b42a42c9c3ecfaa74e177
Martina Muliett <settembrina9@gmail.com>
2021-03-07 09:54:30 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,6 @@
 function callMe() {
   var lyric;
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
+  console.log(lyric);
   var lyric = "maybe";
 }
 
